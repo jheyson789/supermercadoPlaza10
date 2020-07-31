d17f57dbe7bc91ddada787235c87124e11587d5a
jheyson zurita <jheysoria20@gmail.com>
2020-07-31 19:08:07 -0400

    modificaciones del header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public navFixed = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  @HostListener("window:scroll", ['$event'])
+  doSomethingOnWindowsScroll($event:Event){
+    // let scrollOffset = $event.srcElement.children[0].scrollTop;
+    let scrollOffset = document.documentElement.scrollTop;
+    this.topNavegacion(scrollOffset);
+    
+  }
+
+  topNavegacion(posicion: number) {
+    if (posicion > 60) {
+      this.navFixed = true;
+    } else {
+      this.navFixed = false;
+    }
+  }
+
 
 }
