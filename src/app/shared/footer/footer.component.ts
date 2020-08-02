3f4b38bd3d9c0663564b6e8b98ae896eb506a333
jheyson zurita <jheysoria20@gmail.com>
2020-08-01 20:22:39 -0400

    arreglos en el footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-footer',
@@ -10,6 +11,13 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    var mapaLeaflet = L.map('ubicacion').setView([-17.3932357,-66.1588465], 18);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapaLeaflet);
+    L.marker([-17.393174, -66.158819]).addTo(mapaLeaflet)
+    .bindPopup('PLAZA 10 <br> SUPERMERCADO')
+    .openPopup();
 
+  }
 }
